fcef189086e2e8536ac3aa0f6bb643b8b187a689
mbrunelliere <marion.brnllr@gmail.com>
2015-10-26 16:31:26 +0100

    PlayerController

--- Assets/Scripts/ACharacter.cs
+++ Assets/Scripts/ACharacter.cs
@@ -3,13 +3,92 @@ using System.Collections;
 
 public class ACharacter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float walkSpeed = 1;
+    bool _isGrounded = true;
+
+
+    Animator animator;
+
+    enum States {
+        idle = 0,
+        walk = 1,
+        jump = 2
+    };
+
+
+    string _currentDirection = "right";
+    uint _currentAnimationState = (uint)States.idle;
+
+    // Use this for initialization 
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            changeDirection("right");
+            transform.Translate(Vector3.right * walkSpeed * Time.fixedDeltaTime);
+
+            if (_isGrounded)
+            {
+                changeState((uint)States.walk);
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            changeDirection("left");
+            transform.Translate(Vector3.right * walkSpeed * Time.fixedDeltaTime);
+
+            if (_isGrounded)
+            {
+                changeState((uint)States.walk);
+            }
+        }
+        else if (_isGrounded)
+        {
+            changeState((uint)States.idle);
+        }
+    }
+
+    void changeState(uint state)
+    {
+        if (_currentAnimationState == state)
+        {
+            return;
+
+            animator.SetInteger("state", (int)state);
+            _currentAnimationState = state;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "Floor")
+        {
+            _isGrounded = true;
+            changeState((uint)States.idle);
+        }
+        Debug.Log("une collision ...");
+    }
+
+    void changeDirection(string direction)
+    {
+        if (_currentDirection != direction)
+        {
+            if (direction == "right")
+            {
+                transform.Rotate(0, -180, 0);
+            }
+            else if (direction == "left")
+            {
+                transform.Rotate(0, 180, 0);
+            }
+            _currentDirection = direction;
+        }
+    }
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,8 +14,9 @@ public class PlayerScript : MonoBehaviour {
     const uint STATE_WALK = 1;
     const uint STATE_JUMP = 2;
      
-    string _currentDirection = "right";
+    bool _currentDirection = true;
     uint _currentAnimationState = STATE_IDLE;
+
 
     // Use this for initialization 
     void Start () {
@@ -24,33 +25,20 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate ()
-    { 
-	     if (Input.GetKey(KeyCode.RightArrow))
-        {
-            changeDirection("right");
-            transform.Translate(Vector3.right * walkSpeed * Time.fixedDeltaTime);
+    {
+        float move = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-            if (_isGrounded)
-            {
-                changeState(STATE_WALK);
-            }
-
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        if(move > 0 && !_currentDirection)
         {
-            changeDirection("left");
-            transform.Translate(Vector3.right * walkSpeed * Time.fixedDeltaTime);
-
-            if (_isGrounded)
-            {
-                changeState(STATE_WALK);
-            }
+            changeDirection();
         }
-        else if (_isGrounded)
+        if (move < 0 && _currentDirection)
         {
-            changeState(STATE_IDLE);
+            changeDirection();
         }
-    }
+
+    } 
 
     void changeState(uint state)
     {
@@ -73,19 +61,12 @@ public class PlayerScript : MonoBehaviour {
         Debug.Log("une collision ...");
     }
      
-    void changeDirection(string direction)
+    void changeDirection()
     {
-        if (_currentDirection != direction) 
-        {
-            if (direction == "right")
-            {
-                transform.Rotate(0, -180, 0);
-            }
-            else if (direction == "left")
-            {
-                transform.Rotate(0, 180, 0);
-            }
-            _currentDirection = direction;
-        }
+        _currentDirection = !_currentDirection;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
     }
 }
