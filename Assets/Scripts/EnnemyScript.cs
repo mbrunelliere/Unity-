4f2ac2a7354ba9ce12b64d5fb9d5d89d6c345f11
mbrunelliere <marion.brnllr@gmail.com>
2015-10-27 16:30:56 +0100

    Characters controllers

---
+++ Assets/Scripts/EnnemyScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyScript : MonoBehaviour {
+
+    public float walkSpeed = 1;
+
+    const uint STATE_WALK = 1;
+    const uint STATE_DIE = 2;
+
+    Animator animator;
+
+    uint _currentAnimationState = STATE_WALK;
+    string _currentDirection = "left";
+
+    // Use this for initialization
+    void Start () {
+        animator = this.GetComponent<Animator>();
+    }
+	 
+	// Update is called once per frame
+	void FixedUpdate () {
+        Debug.Log(_currentDirection);
+        if (_currentDirection == "left")
+        {
+            transform.Translate(Vector3.left * walkSpeed * Time.fixedDeltaTime);
+        }
+        else if (_currentDirection == "right")
+        {
+            transform.Translate(Vector3.right * walkSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    void changeState(uint state)
+    {
+        if (_currentAnimationState == state)
+        {
+            return;
+        }
+        animator.SetInteger("state", (int)state);
+        _currentAnimationState = state;
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("collision");
+        if(_currentDirection == "left")
+        {
+            changeDirection("right");
+        }
+        else if (_currentDirection == "right")
+        {
+            changeDirection("left");
+        }
+
+        if (coll.gameObject.name == "Player" )
+        {
+            foreach (ContactPoint2D contact in coll.contacts)
+            {
+                if (contact.point.y >= -0.64 && contact.point.x >= 0 && contact.point.x <= 0.52) {
+                    Debug.Log("Mort ennemi");
+                    die();
+                }
+            }
+        }
+         
+    }
+
+    void changeDirection(string direction) 
+    {
+        if (_currentDirection != direction)
+        {
+            Vector3 theScale = transform.localScale;
+            if (direction == "left")
+            {
+                theScale.x = -1;
+            }
+            else if (direction == "right")
+            {
+                theScale.x = 1;
+            }
+            transform.localScale = theScale;
+            _currentDirection = direction;
+        }
+    }
+
+    void die()
+    {
+        changeState(STATE_DIE);
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,49 +6,117 @@ public class PlayerScript : MonoBehaviour {
     public float walkSpeed = 1;
 
     bool _isGrounded = true;
-
+    bool _isCrounching = false;
+    bool _islanding = false;
+    bool _isJumping = false;
+    bool _isFalling = false;
 
     Animator animator;
+    Bounds bounds;
+    GameObject map;
 
     const uint STATE_IDLE = 0;
     const uint STATE_WALK = 1;
     const uint STATE_JUMP = 2;
-     
-    bool _currentDirection = true;
+    const uint STATE_CROUNCH = 3;
+    const uint STATE_LAND = 4;
+    const uint STATE_FALL = 5;
+    const uint STATE_HURT = 6;
+
+    string _currentDirection = "left";
     uint _currentAnimationState = STATE_IDLE;
+  
 
-
-    // Use this for initialization 
-    void Start () {
-        animator = this.GetComponent<Animator>();
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+    // Use this for initialization
+    void Start()
     {
-        float move = Input.GetAxis("Horizontal");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(move * walkSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        animator = this.GetComponent<Animator>();
+        map = GameObject.FindWithTag("Map");
+        //bounds = map.GetComponent<SpriteRenderer>().sprite.bounds;
+        bounds = map.GetComponent<Renderer>().bounds;
+    }
 
-        if(move > 0 && !_currentDirection)
+    // Update is called once per frame
+    void FixedUpdate() 
+    {
+        //Camera Movement
+        Camera cam = GetComponent<Camera>();
+
+        float leftBound = bounds.min.x;
+        float rightBound = bounds.max.x;
+        float bottomBound = bounds.min.y;
+        float topBound = bounds.max.y;
+        //Debug.Log(leftBound + " " + rightBound + " " + bottomBound + " " + topBound);
+
+        float camX = Mathf.Clamp(transform.position.x, leftBound, rightBound);
+        float camY = Mathf.Clamp(transform.position.y, bottomBound, topBound);
+
+        cam.transform.position = new Vector3(camX, camY, cam.transform.position.z);
+
+
+        //Player Movement
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            changeDirection();
+            if (_isGrounded)
+            {
+                changeState(STATE_LAND); 
+            }
         }
-        if (move < 0 && _currentDirection)
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
-            changeDirection();
+            if (_isGrounded)
+            {
+                _isGrounded = false;
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 250));
+                changeState(STATE_JUMP);
+            }
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && !_isCrounching)
+        {
+            changeDirection("right");
+            transform.Translate(Vector3.right * walkSpeed * Time.fixedDeltaTime);
+
+            if (_isGrounded)
+            {
+                changeState(STATE_WALK);
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && !_isCrounching )
+        {
+            changeDirection("left");
+            transform.Translate(Vector3.left * walkSpeed * Time.fixedDeltaTime);
+
+            if (_isGrounded)
+            {
+                changeState(STATE_WALK);
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            changeState(STATE_CROUNCH);
+        }
+        else if (_isGrounded)
+        {
+            changeState(STATE_IDLE);
+        }
+        else if (!_isGrounded)
+        {
+            changeState(STATE_FALL);
         }
 
-    } 
+        _isCrounching = animator.GetCurrentAnimatorStateInfo(0).IsName("Patch_duck");
+    }
 
     void changeState(uint state)
     {
         if (_currentAnimationState == state)
         {
-            return; 
-
-            animator.SetInteger("state", (int)state);
-            _currentAnimationState = state;
+            return;
         }
+        animator.SetInteger("state", (int)state);
+        _currentAnimationState = state;
+          
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -57,16 +125,32 @@ public class PlayerScript : MonoBehaviour {
         {
             _isGrounded = true;
             changeState(STATE_IDLE);
+            Debug.Log("une collision ...");
         }
-        Debug.Log("une collision ...");
-    }
-     
-    void changeDirection()
-    {
-        _currentDirection = !_currentDirection;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
 
+        if (coll.gameObject.name == "Ennemy")
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(150, 150));
+            changeState(STATE_HURT);
+        }
+
+    }
+
+    void changeDirection(string direction)
+    {
+        if (_currentDirection != direction)
+        {
+            Vector3 theScale = transform.localScale;
+            if (_currentDirection == "left")
+            {
+                theScale.x = 1;
+            }
+            if (_currentDirection == "right")
+            {
+                theScale.x = -1;
+            }
+            transform.localScale = theScale;
+            _currentDirection = direction;
+        }
     }
 }
