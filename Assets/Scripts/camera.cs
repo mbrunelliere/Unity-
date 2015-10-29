057dc6056bedfcf6d195e1733942131a947b1664
mbrunelliere <marion.brnllr@gmail.com>
2015-10-29 10:43:15 +0100

    Camera follow Player

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -5,23 +5,32 @@ public class camera : MonoBehaviour {
 
     Bounds bounds;
     GameObject map;
+    GameObject player;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
 
     // Use this for initialization
     void Start () {
         map = GameObject.FindWithTag("Map");
         bounds = map.GetComponent<Renderer>().bounds;
+        player = GameObject.FindWithTag("Player");
     }
 	
 	// Update is called once per frame
 	void LateUpdate () {
 
-        float leftBound = bounds.min.x;
-        float rightBound = bounds.max.x;
-        float bottomBound = bounds.min.y;
-        float topBound = bounds.max.y;
+        Vector3 worldToScreen = Camera.main.ScreenToWorldPoint(transform.position);
 
-        float camX = Mathf.Clamp(transform.position.x, leftBound, rightBound);
-        float camY = Mathf.Clamp(transform.position.y, bottomBound, topBound);
+        float leftBound = bounds.min.x + 4 ;
+        float rightBound = bounds.max.x - 4;
+        float bottomBound = bounds.min.y + 1.8f; 
+        float topBound = bounds.max.y - 1.8f;
+          
+        float camX = Mathf.Clamp(player.transform.position.x, leftBound, rightBound);
+        float camY = Mathf.Clamp(player.transform.position.y, bottomBound , topBound);
 
         transform.position = new Vector3(camX, camY, transform.position.z);
     }
